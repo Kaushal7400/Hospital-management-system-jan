0615b001ff3c29e5d6e60d11e68318efa613b953
Kaushalendra singh <ksb250998@gmail.com>
2024-01-19 10:51:26 +0530

    Admit patient got created

--- src/Models/Room.java
+++ src/Models/Room.java
@@ -6,10 +6,52 @@ public class Room {
     Patient patient;
     Doctor doctor;
 
+    public String getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
     public Room(String roomID, boolean isOccupied, Patient patient, Doctor doctor) {
         this.roomID = roomID;
         this.isOccupied = isOccupied;
         this.patient = patient;
         this.doctor = doctor;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomID='" + roomID + '\'' +
+                ", isOccupied=" + isOccupied +
+                ", patient=" + patient +
+                ", doctor=" + doctor +
+                '}';
     }
 }
--- src/database/RoomDatabase.java
+++ src/database/RoomDatabase.java
@@ -6,7 +6,25 @@ import java.util.HashMap;
 
 public class RoomDatabase {
     HashMap<String, Room> roomDB;
-    RoomDatabase(){
+    public RoomDatabase(){
         this.roomDB = new HashMap<>();
+    }
+
+    public Room getEmptyRoom(){
+        for(String key:roomDB.keySet()){
+            Room room = roomDB.get(key);
+            if(!room.isOccupied()) {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public int getTotalRooms(){
+        return roomDB.size();
+    }
+    public void addRoomToDB(Room room){
+        String roomId = room.getRoomID();
+        roomDB.put(roomId,room);
     }
 }
